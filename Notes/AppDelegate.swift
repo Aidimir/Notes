3ac7fff7ee1ccf9262ea6e4f9968cea97b3f61c3
Aidimir <aidimir1@mail.ru>
2023-02-15 15:08:59 +0500

    Only portrait mode enabled. Forgot to add it from the start.

--- Notes/AppDelegate.swift
+++ Notes/AppDelegate.swift
@@ -10,8 +10,10 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
